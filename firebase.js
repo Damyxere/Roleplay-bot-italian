10aa5121311466b37893db10ff4243550d1cb02f
scorpion <143728714+Damyxere@users.noreply.github.com>
2026-05-26 13:10:20 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,30 @@
-
-
 const admin = require('firebase-admin');
+let db;
 
-// Incolla qui dentro ESATTAMENTE i dati del file JSON che hai scaricato
-const serviceAccount = {
-  
-  "type": "service_account",
-  "project_id": "roleplay-bot-italian",
-  "private_key_id": "2826a53926fea2e87e23e8f0fe331f5a82bd13bf",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDJD57YZF4Rhl58\nbslyZLdyozHSMlQu8zj+80y9eerwAY9ZPFBfgZtT8m26kh98YBfBLQoSLqrWfneN\nCFxq4aDCfI+JqVu/iNkdr/g68+jkHNIC9JQ8IGPBktlqO71pWilzzs6CD4wxGoBC\nSScBTGAGeMQwjBoUSpU48mmL/aYAhucNalCtjRKqEJ7beiV0XK4HP5y9yizNLzdV\nCD7Zmr3BZnHyWPvp6vNQ/lYQH2sAaAmBddGpf6DNbZLvcIZqFv7Q6aC7+cuLP34P\nIPvKADA8VKwWDCOO1jvco8YAZo+gkCdohklrZ1euat3zcVJPMzHcV7rnbIVBs5Gk\nnHVXeJHtAgMBAAECggEABJ1pa7WYDCZtpkUDti62IDk7NEQn9wDpqr5qYUH6YMJk\naCuwe5S1vD5JAvqGIvxwpyQxwjuUe8Jg25mYx3JA1p31zHbY7oNFWn9WvwGos42u\ny1cSTPQAXinQ+DWBUjj50Rjq2ATj1p+xWHQl40t9u8BLXoLkuVkB5lH4Fri+de4Y\nVExDmlhEqpvCrxXsgNeMKwpSvbZ9CpIv3/ibpSAjSP3sCOLUvHXFtNhhrrqYcppU\nYCWY9km6d3VNOWLUw2ryEcTEV6z8oxZyD2ACSOJXjV2jLcVTdag8CYrAAbIOApPz\nOx+aipfitMwpL26k4M+AEqQeMt6cYlQWFCrNwh7IyQKBgQDtCBFgFHLs6CDIJY4l\nJgstXEfTNekV6JSeVkAP4X2atJICIoiT7MD7pPgnHrX5Man30re+DVSdWoSEEZIO\nFg3Q3RYdgF6F8SJjfGY5ZbBaUrFC9rfVBPtPfCfpZTuOQRRUz51XExNK4apgC0la\nnHMv5sT6hPmFwy3HkJJQlpvuBQKBgQDZJqUoJQ7p1MEzcd9OrFYuU36cTFFz+Uj1\n+WGrjAqEVL11E95BZIHL/+j99CFMgODgHtBEjrvMEIpnixt/j7FIChAqG8S+YLR1\nLzPRg4/LEhFB00xwaHdKL8MeQ5j+oKDpr/SeaRqas9eusQC60o17kxbMo9Ub1xvS\noSR+By7wyQKBgQC7zNmfa3RHGf/nNEI0UrkmXmgWAy/DCPy/mwy0yG7xJUJN6LiX\no0yf1Ow8aou5fczyKoUkUoGJDs/Rp+41tOmLY7AC/dNmglBBzK1dqqEYX05NBRdP\n8tt+9ZrVxDx0kEGglMJP3DA78yI/aJHc9foZqNjGuJbOkX7zECutKDcxVQKBgEVc\nD6K7+4B6wV8yPxZ8+2X+q1E3G0lE1aYjiqvlpjJQctdjijku5jqHuh7rjHtIKwvL\nK9DKGnf6zIp4J0deWUzeUf1xMy1v7BQGCLIiKJq/YZ255Q9+fqPGj7cp9kNWgS9D\nUYoLnkDNstWMmeQSfrh66exhZB9pXOyxJdvPZZHpAoGAd0PaXUJujx2nWsu77vY9\nGJnhubzjAy+v4nNa4+4dSn6sWxmAngnEbS3ogGYDwN9/crF8RpxZfwtz8ZScKwWN\n4jHtFXFNihwnDcau9gOfFCeBfTfq3k16zQlHSz1byxyz9kdQzs8kqOVBFGRg3R8K\nD/IXX8Gpf0sf0IEp/kUZa7g=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@roleplay-bot-italian.iam.gserviceaccount.com",
-  "client_id": "105572112829507681413",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40roleplay-bot-italian.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-};
+try {
+    const firebaseConfigRaw = process.env.FIREBASE_CONFIG;
+    if (!firebaseConfigRaw) throw new Error("Manca FIREBASE_CONFIG su Render");
 
-if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
+    const serviceAccount = JSON.parse(firebaseConfigRaw);
+
+    if (!admin.apps.length) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+    }
+    db = admin.firestore();
+    console.log("🔥 Firebase connesso tramite Variabili d'Ambiente!");
+} catch (error) {
+    console.error(error.message);
+    db = {
+        collection: () => ({
+            doc: () => ({
+                get: async () => ({ exists: false, data: () => ({}) }),
+                set: async () => true,
+                update: async () => true
+            })
+        })
+    };
 }
 
-const db = admin.firestore();
 module.exports = db;
