520197b16e7d82ebe38e650f5233b081785e0f9f
scorpion <143728714+Damyxere@users.noreply.github.com>
2026-06-12 22:15:11 +0200

    Update dbManager.js

--- dbManager.js
+++ dbManager.js
@@ -1,20 +1,15 @@
-const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-
-// Carica la configurazione
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-initializeApp({ credential: cert(serviceAccount) });
 const db = getFirestore();
 
-console.log("✅ Database connesso.");
-
 module.exports = {
-    db,
-    haDocumento: async (g, u) => (await db.collection('documenti').doc(`${g}_${u}`).get()).exists,
+    // Legge il PIN dell'utente specifico nel server specifico
     getPin: async (g, u) => {
         const doc = await db.collection('telefoni').doc(`${g}_${u}`).get();
         return doc.exists ? doc.data().pin : null;
     },
+    // Controlla se l'utente ha un documento attivo
+    haDocumento: async (g, u) => (await db.collection('documenti').doc(`${g}_${u}`).get()).exists,
+    // Controlla se è premium
     isPremium: async (g, u) => {
         const doc = await db.collection('documenti').doc(`${g}_${u}`).get();
         return doc.exists && doc.data().premium === true;
