259e3621da422dc1947971e59d9c22feaa2d6a2b
scorpion <143728714+Damyxere@users.noreply.github.com>
2026-06-12 21:35:51 +0200

    Update dbManager.js

--- dbManager.js
+++ dbManager.js
@@ -1,16 +1,16 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-// Caricamento sicuro dalla variabile d'ambiente
+// Carica la configurazione
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
 initializeApp({ credential: cert(serviceAccount) });
 const db = getFirestore();
+
+console.log("✅ Database connesso.");
 
 module.exports = {
     db,
     haDocumento: async (g, u) => (await db.collection('documenti').doc(`${g}_${u}`).get()).exists,
-    setPin: async (g, u, p) => await db.collection('telefoni').doc(`${g}_${u}`).set({ pin: p }, { merge: true }),
     getPin: async (g, u) => {
         const doc = await db.collection('telefoni').doc(`${g}_${u}`).get();
         return doc.exists ? doc.data().pin : null;
